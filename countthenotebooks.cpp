e2961f58b6bc8fc80b055684ead8c19b3f9a4a4c
Soham Rane <ranesoham007@gmail.com>
2025-07-08 07:44:34 +0530

    Create countthenotebooks.cpp

---
+++ countthenotebooks.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; // Read the number of test cases
+
+    while (t--) {
+        int n;
+        cin >> n; // Read the weight of the pulp
+
+        cout << n * 10 << endl; // Calculate and print the number of notebooks
+    }
+
+    return 0;
+}
